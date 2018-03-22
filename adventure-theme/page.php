8100ab1c1eacadb1cb2b059712b204b911bf765f
Avery Thomas <averyethomas@gmail.com>
2018-03-22 17:50:57 -0400

    add photo layout options to single page template

--- adventure-theme/page.php
+++ adventure-theme/page.php
@@ -9,13 +9,47 @@
     while ( have_posts() ) : the_post();
 ?>
 
-<div ng-cloak class="content-container container">
+<div ng-cloak class="content-container container single-page">
     <h1><?php the_title(); ?></h1>
     <div class="content">
-        <?php the_content(); ?>
+<?php   the_content(); 
+        if( have_rows('intro_images') ):
+            while( have_rows('intro_images') ): the_row(); 
+                  if( get_row_layout() == 'single' ):
+                        $image = get_sub_field('image');
+                        echo '<div class="single">';
+                        echo '<img src="' . $image['url'] . '" alt="' . $image['alt'] . '" />';
+                        echo '</div>';
+                  endif;
+                  if( get_row_layout() == 'double' ):
+                        $imageLeft = get_sub_field('image_left');
+                        $imageRight = get_sub_field('image_right');
+                        echo '<div class="double">';
+                        echo '<img src="' . $imageLeft['url'] . '" alt="' . $imageLeft['alt'] . '" />';
+                        echo '<img src="' . $imageRight['url'] . '" alt="' . $imageRight['alt'] . '" />';                              
+                        echo '</div>';
+                  endif;
+                  if( get_row_layout() == 'triple' ):
+                        $imageLeft = get_sub_field('image_left');
+                        $imageRight = get_sub_field('image_right');
+                        $imageCenter = get_sub_field('image_center');
+                        echo '<div class="triple">';
+                        echo '<img src="' . $imageLeft['url'] . '" alt="' . $imageLeft['alt'] . '" />';
+                        echo '<img src="' . $imageCenter['url'] . '" alt="' . $imageCenter['alt'] . '" />';                              
+                        echo '<img src="' . $imageRight['url'] . '" alt="' . $imageRight['alt'] . '" />';                              
+                        echo '</div>';
+                  endif;
+                  if( get_row_layout() == 'tall' ):
+                        $image = get_sub_field('image');
+                        echo '<div class="single">';
+                        echo '<img src="' . $image['url'] . '" alt="' . $image['alt'] . '" />';
+                        echo '</div>';
+                  endif;
+            endwhile;
+      endif; 
+?>
     </div>
 </div>
-
 <?php   endwhile;
         get_footer();
 ?>
